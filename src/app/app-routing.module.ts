f11e2c30cd3dcb245cf32c6461e93b7d23e26150
shawin <shawinpradhan@gmail.com>
2023-11-28 15:23:53 +0530

    new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DonateComponent } from './donate/donate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'donate', component:DonateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { DonateComponent } from './donate/donate.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule,
